0a6a17201f6d06775c77b051ef19e59fe931d10c
joethyr <joethayr@gmail.com>
2022-07-19 15:10:16 -0600

    add constructor and function to add book object to library array

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 let myLibrary = [];
 
-function Book(title, author, isbn) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
-  this.isbn = isbn
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(title, author, pages, read) {
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
 }
