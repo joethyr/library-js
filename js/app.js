1fb04b1fc08ec35f196830e1ca1c420e40493979
joethyr <joethayr@gmail.com>
2022-07-19 21:59:05 -0600

    add function that saves user form inputs

--- js/app.js
+++ js/app.js
@@ -1,24 +1,32 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('titleBook')
+// fixes autofocus on Modal
+const myModal = document.getElementById('myModal');
+const myInput = document.getElementById('titleBook');
 
 myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
+  myInput.focus();
+});
 
 // add books to library
-let myLibrary = [];
+const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+// saves user form inputs
+function saveBook() {
+  const titleBook = document.getElementById('titleBook').value;
+  const authorBook = document.getElementById('authorBook').value;
+  const pagesBook = document.getElementById('pagesBook').value;
+  // const readBook = document.getElementById('readBook');
+  addBookToLibrary(titleBook, authorBook, pagesBook);
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  let newBook = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages) {
+  let newBook = new Book(title, author, pages);
   myLibrary.push(newBook);
 }
 
-// display books in table
-console.log(addBookToLibrary('jojo', 'bobo', 123, yes));
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  // this.read = read;
+}
+
